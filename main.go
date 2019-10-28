6ff92b1def8adcbd06594dc512519ea08dea6d98
site0801 <skuroneko0801@gmail.com>
2019-10-28 11:55:29 +0900

    test jwt

--- main.go
+++ main.go
@@ -2,13 +2,91 @@ package main
 
 import (
     "net/http"
+    "time"
+
+    "github.com/dgrijalva/jwt-go"
     "github.com/labstack/echo"
+    "github.com/labstack/echo/middleware"
 )
+
+// jwtCustomClaims are custom claims extending default ones.
+type jwtCustomClaims struct {
+	Name  string `json:"name"`
+	Admin bool   `json:"admin"`
+	jwt.StandardClaims
+}
+
+func hello(c echo.Context) error {
+    return c.JSON(http.StatusOK, map[string]string{"hello": "World"})
+}
+
+func login(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	// Throws unauthorized error
+	if username != "jon" || password != "shhh!" {
+		return echo.ErrUnauthorized
+	}
+
+	// Set custom claims
+	claims := &jwtCustomClaims{
+		"Jon Snow",
+		true,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+		},
+	}
+
+	// Create token with claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"token": t,
+	})
+}
+
+func accessible(c echo.Context) error {
+	return c.String(http.StatusOK, "Accessible")
+}
+
+func restricted(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(*jwtCustomClaims)
+	name := claims.Name
+	return c.String(http.StatusOK, "Welcome "+name+"!")
+}
 
 func main() {
     e := echo.New()
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
+
+    // Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+    //Login route
+    e.POST("/login", login)
+
+    e.GET("/jwt-test", accessible)
+
+    r := e.Group("/restricted")
+
+    config := middleware.JWTConfig{
+		Claims:     &jwtCustomClaims{},
+		SigningKey: []byte("secret"),
+	}
+	r.Use(middleware.JWTWithConfig(config))
+	r.GET("", restricted)
+
+
+    e.GET("/", hello)
+    
+    
     e.Logger.Fatal(e.Start(":1323"))
 }
