91d666905b3bbe70141b9346dc894e4d1268bc7e
site0801 <skuroneko0801@gmail.com>
2019-11-19 13:36:23 +0900

    create public directory

--- main.go
+++ main.go
@@ -19,6 +19,22 @@ func main() {
 	e.Use(middleware.Logger())
     e.Use(middleware.Recover())
     e.Use(handler.BasicAuth())
+    e.Static("/assets", "public/assets")
+
+
+    e.File("/", "public/index.html")
+    e.File("/signup", "public/signup.html")
+    e.POST("/signup", handler.Signup)
+    e.File("/login", "public/login.html")
+    e.POST("/login", handler.Login)
+
+
+    api := e.Group("/api")
+    api.Use(middleware.JWTWithConfig(handler.Config))
+    api.GET("/todos", handler.GetTodos)
+    api.POST("/todos", handler.AddTodo)
+    api.DELETE("/todos/:id", handler.DeleteTodo)
+    api.PUT("/todos/:id/completed", handler.UpdateTodo)
 
     e.GET("/", hello)
     e.Logger.Fatal(e.Start(":1323"))
